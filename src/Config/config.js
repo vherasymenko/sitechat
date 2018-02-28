c5a2ac2936249ee185fa3d1d74615688a356a8ef
Volodymyr Herasymenko <vovagerasimenko.zet@gmail.com>
2018-02-28 15:25:39 +0200

    Delete config.js

--- src/Config/config.js
+++
@@ -1,8 +0,0 @@
-export const DB_CONFIG = {
-    apiKey: "AIzaSyCLwTjZVafQYRa8Lzgj09MF1fw0gIvMiZA",
-    authDomain: "socialchat-2b671.firebaseapp.com",
-    databaseURL: "https://socialchat-2b671.firebaseio.com",
-    projectId: "socialchat-2b671",
-    storageBucket: "socialchat-2b671.appspot.com",
-    messagingSenderId: "904175877778"
-};
